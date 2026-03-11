7f5085aee185fb644e4fe6b27206ff93eb6152cb
Vinraj1 <160110182+Vinraj1@users.noreply.github.com>
2026-03-11 00:58:57 +0000

    Renaming requirements

--- Music/settings.py
+++ Music/settings.py
@@ -126,6 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
